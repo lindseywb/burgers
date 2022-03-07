44c097e2fd5357d88c00641a891ec3f4ca92c010
Lindsey Bell <lindseywb@gmail.com>
2022-03-07 17:01:05 -0500

    Escape the apostrophe

--- components/global/Heading/Heading.js
+++ components/global/Heading/Heading.js
@@ -3,7 +3,7 @@ import styles from './Heading.module.css'
 export default function Heading() {
     return(
         <header className={styles.Heading}>
-            <h1>It's Burger Time</h1>
+            <h1>It&apos;s Burger Time</h1>
         </header>
     )
 }
